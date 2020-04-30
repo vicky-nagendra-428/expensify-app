d921dd0e555fe1e764540846d34fed34ee3bdb77
Varma <nvarma@c02yx0khlvdq.agoda.local>
2020-05-01 01:25:52 +0700

    update price display

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 export const ExpenseListItem = ({id, description, amount, createdAt}) => (
     <div>
         <p>
             <Link to={`/edit/${id}`}>{description}</Link>
         </p>
-        <p>{amount}</p>
-        <p>{createdAt}</p>
+        <p>{numeral(amount / 100).format('$0,0.00')}</p>
+        <p>{moment(createdAt).format('MMMM Do YYYY')}</p>
        
     </div>
 );
--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { ExpenseListItem } from '../../components/ExpenseListItem';
+import moment from 'moment';
 
 test('expense list item', () => {
     const item = {
         id: '1',
         description: 'description',
         amount: 120,
-        createdAt: 0
+        createdAt: 100
     }
     const wrapper = shallow(<ExpenseListItem {...item}/>);
     expect(wrapper).toMatchSnapshot();
