46aabd40dfd9dfc003d678a312452e4f1470276e
Varma <nvarma@c02yx0khlvdq.agoda.local>
2020-05-01 22:16:07 +0700

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const MiniCssExtract = require('mini-css-extract-plugin');
-import webpack from 'webpack';
+const webpack = require('webpack');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
